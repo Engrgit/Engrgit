85e1eb74bdc0a03605efe430c9d219d0d99a05fc
Ibrahim Gbadegesin <ibrahimgbadegesin@gmail.com>
2021-02-24 05:42:13 +0100

    derivationscript

---
+++ Derivation.cpp
@@ -0,0 +1,61 @@
+
+          /* My surname is Gbadegesin. It is a royal name in Yoruba land which literally means "to take the crown and climb a horse". 
+             In the olden days the best form of  transportation is via horses. Anybody that can take the crown and climb a horse must be a powerful ruler.
+
+             "Gbe" = Take
+             "Ade" = Crown
+             "Gunesin" = Climb an horse
+
+             "Gbeadegunesin" = "Gbadegesin"       //due to short form abbreviation of words for quick pronunciation. 
+              Hence,
+             "Gunesin" = "Gesin" = climb an horse */
+
+
+void assumptions()
+{
+
+            //Am an engineer, Short forms are permitted, the final nickname should be a one word that embodies my name and my attributes
+}
+
+void methodology
+{
+  std::strings fullname, nickname, title;      // variables in strings:  fullname, nickname, title
+
+  std::string title;           // Title-  My education and skills proves that am an Engineer. Since short form is allowed .
+
+  title = "Engr";             //Engineer = Engr.
+
+  Fullname-  shows my complete identity =  Gbadegesin Ibrahim Tosin
+
+
+ 
+  assumptions();
+// Executing assumptions gives
+  /*However, since short form is allowed. Reducting Each string in the name to a character for quick representation  would mean Gbadegesin = "G",  Ibrahim = "I",       Tosin = "T"
+*/
+ 
+}
+
+void generate(std::string name){
+   std::string fullname = "G" + "I" + "T"
+
+   std::cout<< fullname << std::endl;
+
+   std::string nickname =  title + fullname
+
+   nickname = Engr + GIT
+
+   std::cout<< nickname << std::endl;
+
+   lowercase(nickname) // Applying short form to nickname yields  lowercase(nickname) = Engrgit
+
+   std::cout<< Nickname << std::endl; //Therefore Nickname = Engrgit
+}
+
+
+int main()
+{
+   generate(nickname);
+   return 0;
+}
+
